f323d40d0e8ffbd99debda0f566c1cb733252c9c
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-08 15:57:37 -0800

    event listeners now take in correctspot and handleguesses, created handleGuess function that takes in the user guess and correct spot, if the user guesses the correct spot correct guess will increment, also updated dom to reflect the new state.

--- app.js
+++ app.js
@@ -21,31 +21,60 @@ let totalGuesses = 0;
 
 //set event listeners
 cupButtonOne.addEventListener('click', () => {
+    let correctSpot = getRandomHidingSpot();
 
+    handleGuess('one', correctSpot);
 });
 
 cupButtonTwo.addEventListener('click', () => {
+    let correctSpot = getRandomHidingSpot();
 
+    handleGuess('two', correctSpot);
 });
 
 cupButtonThree.addEventListener('click', () => {
+    let correctSpot = getRandomHidingSpot();
 
+    handleGuess('three', correctSpot);
 });
 
 //create function with random hiding spot
 function getRandomHidingSpot() {
-  const hidingPlaces = [
-    'one',
-    'two',
-    'three'
-  ];
+    const hidingPlaces = [
+        'one',
+        'two',
+        'three'
+    ];
 
-  const index = Math.floor(Math.random() * hidingPlaces.length);
-  return hidingPlaces[index];
+    const index = Math.floor(Math.random() * hidingPlaces.length);
+    return hidingPlaces[index];
 }
 
 //create function that handles guesses
+function handleGuess(userGuess, correctSpot) {
+  //First remove the ball cup from hiding places
+    cupOne.classList.remove('ball-cup');
+    cupTwo.classList.remove('ball-cup');
+    cupThree.classList.remove('ball-cup');
 
+  //increment guesses
+    totalGuesses++;
+
+    const correctCupEl = document.getElementById(`cup-${correctSpot}`);
+    correctCupEl.classList.add('ball-cup');
+
+    
+
+  //if statement: if user is correct, increment correct gueses
+    if (userGuess === correctSpot) {
+        correctGuesses++;
+    }
+
+  //update DOM to reflect the new state
+    winsEl.textContent = correctGuesses;
+    totalEl.textContent = totalGuesses;
+    lossesEl.textContent = totalGuesses - correctGuesses;
+}
 
 // set event listeners 
   // get user input
